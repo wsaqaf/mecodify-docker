8367f5c6f8e9ecb6ed30b7ed8523b8083373c152
Walid Al-Saqaf <wsaqaf@gmail.com>
2017-03-14 18:08:41 +0100

    Update create_mysql_users.sh

--- create_mysql_users.sh
+++ create_mysql_users.sh
@@ -31,10 +31,10 @@ if [ "$CREATE_MYSQL_USER" = true ]; then
     _userdb=${MYSQL_USER_DB:-db}
     _userpass=${MYSQL_USER_PASS:-password}
 
-    mysql -uroot -e "CREATE USER '${_user}'@'%' IDENTIFIED BY  '${_userpass}'"
-    mysql -uroot -e "GRANT USAGE ON *.* TO  '${_user}'@'%' IDENTIFIED BY '${_userpass}'"
+    mysql -uroot -e "CREATE USER '${_user}'@'localhost' IDENTIFIED BY  '${_userpass}'"
+    mysql -uroot -e "GRANT USAGE ON *.* TO  '${_user}'@'localhost' IDENTIFIED BY '${_userpass}'"
     mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${_userdb}"
-    mysql -uroot -e "GRANT ALL PRIVILEGES ON ${_userdb}.* TO '${_user}'@'%'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON ${_userdb}.* TO '${_user}'@'localhost'"
 fi
 
 mysqladmin -uroot shutdown
