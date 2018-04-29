e107ea26a97c92fb951a7de81be9f72dc228031c
Walid Al-Saqaf <walid@al-saqaf.se>
2018-04-29 20:14:07 +0000

    improved user interface

--- go.sh
+++ go.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-docker kill `docker ps -aqf "name=mecodify"`;
-docker rm `docker ps -aqf "name=mecodify"`;
+docker kill `docker ps -aqf "name=mecodify"` &>/dev/null;
+docker rm `docker ps -aqf "name=mecodify"` &>/dev/null;
 docker run -d -i -t -p "80:80" -p "3306:3306" -v ${PWD}/mysql:/var/lib/mysql -v ${PWD}/app:/app --name mecodify wsaqaf/mecodify;
-docker cp mecodify/configurations.php `docker ps -aqf "name=mecodify"`:/var/www/html
+docker cp mecodify/configurations.php `docker ps -aqf "name=mecodify"`:/var/www/html &>/dev/null;
+printf "Initializing and running server. Please wait.";
+
+while ! curl --output /dev/null --silent --head --fail localhost
+        do printf "."
+        sleep 1
+done
+printf " Installation complete!\n\nOpen http://127.0.0.1 with your browser to access the Mecodify platform.\n";
