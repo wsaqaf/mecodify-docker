d97f58d1920f97d2ca9310815bc4fb23f108a84c
Walid Al-Saqaf <wsaqaf@gmail.com>
2021-07-31 13:08:29 +0200

    Updates and fixes

--- go.sh
+++ go.sh
@@ -11,7 +11,7 @@ FILE="${PWD}/mecodify/configurations.php"
   if grep -Eq '"bearer"[[:space:]]*=>[[:space:]]*""' $FILE; then
      printf "It appears that you have not added the Twitter API 2.0 BEARER token into the configurations file.\n"
      while true; do
-         read -p "Do you have a Twitter API premium account (e.g., based on an Academic license)? [y/n]: " yn
+         read -p "Do you have a Twitter API premium account (e.g., based on an Academic license)? [Y/n]: " yn
          case $yn in
       		[Yy]* )
     			  printf "Setting the license type to premium...\n";
@@ -21,11 +21,11 @@ FILE="${PWD}/mecodify/configurations.php"
 		          printf "Setting the license type to sandbox (limited)...\n";
             sed -i '' 's/"is_premium"[[:blank:]]*=>[[:blank:]]*true/"is_premium" => false/' $FILE;
             break;;
-      		* ) printf "Please answer yes or no.\n";
+      		* ) printf "Please answer with y or n.\n";
    		  esac
       done
       printf "Please copy the full BEARER token and paste it below. This will only be required once.\n"
-      read -rp "BEARER token: " token
+      read -rp "BEARER token: "token
       if [ ${#token} -ge 10 ]; then
             etoken=$(printf '%s\n' "$token" | sed -e 's/[\/&]/\\&/g')
             sed -i '' 's/"bearer"[[:blank:]]*=>[[:blank:]]*""[[:blank:]]*,/"bearer" => "'"$etoken"'",/' $FILE;
@@ -45,7 +45,7 @@ FILE="${PWD}/mecodify/configurations.php"
   if [ -d "${PWD}/mysql" ]; then
       printf "You have an existing mysql Mecodify database.\n"
       while true; do
-  	read -rp "Do you wish to keep or delete it? Enter y to keep, n to delete: " yn
+  	read -rp "Do you wish to keep or delete it? Enter y to keep, n to delete: [Y/n]" -i "y" yn
   	case $yn in
   	   [Yy]* ) rm -rf "${PWD}/mysql"; break;;
   	   [Nn]* ) break;;
